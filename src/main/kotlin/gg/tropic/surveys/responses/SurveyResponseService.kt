1556236bcd5e70bd293bd0ac7f1b42f861ca2504
Max <67809373+98ping@users.noreply.github.com>
2024-01-02 00:34:54 -0800

    fix: make some services work

--- src/main/kotlin/gg/tropic/surveys/responses/SurveyResponseService.kt
+++ src/main/kotlin/gg/tropic/surveys/responses/SurveyResponseService.kt
@@ -1,7 +1,9 @@
 package gg.tropic.surveys.responses
 
+import gg.scala.flavor.service.Service
 import gg.scala.store.controller.DataStoreObjectControllerCache
 
+@Service
 object SurveyResponseService
 {
     fun create(response: SurveyResponse) = DataStoreObjectControllerCache
--- src/main/kotlin/gg/tropic/surveys/survey/SurveyService.kt
+++ src/main/kotlin/gg/tropic/surveys/survey/SurveyService.kt
@@ -2,7 +2,9 @@ package gg.tropic.surveys.survey
 
 import gg.scala.commons.persist.datasync.DataSyncService
 import gg.scala.commons.persist.datasync.DataSyncSource
+import gg.scala.flavor.service.Service
 
+@Service
 object SurveyService : DataSyncService<SurveyContainer>()
 {
     override fun keys() = SurveyKeys
